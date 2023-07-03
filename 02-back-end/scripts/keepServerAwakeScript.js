033d064b0eb6b6fde4f09d824575e6b26542cc90
Alen Mujanovic <alen.mujanovicc@gmail.com>
2023-07-03 22:28:14 +0200

    Test cron job for keeping server running

--- 02-back-end/scripts/keepServerAwakeScript.js
+++ 02-back-end/scripts/keepServerAwakeScript.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const http = require('http');
 
-cron.schedule('*/14 * * * *', () => {
+cron.schedule('*/10 * * * *', () => {
   console.log('Sending a self-ping to the server');
   http.get('https://digital-dental-office-server.onrender.com');
 });
