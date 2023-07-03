3cbd87b74d16e3c7f3c88d2a169bea7cc0672a85
Alen Mujanovic <alen.mujanovicc@gmail.com>
2023-07-03 23:17:35 +0200

    test

--- 02-back-end/scripts/keepServerAwakeScript.js
+++ 02-back-end/scripts/keepServerAwakeScript.js
@@ -1,7 +1,31 @@
 const cron = require('node-cron');
-const http = require('http');
+const https = require('https');
 
-cron.schedule('*/10 * * * *', () => {
-  console.log('Sending a self-ping to the server');
-  http.get('https://digital-dental-office-server.onrender.com');
+exports.handler = async (event, context) => {
+  const url = 'https://digital-dental-office-server.onrender.com/api/v1';
+
+  return new Promise((resolve, reject) => {
+    const req = https.get(url, (res) => {
+      if (res) {
+        resolve({
+          body: 'Server pinged successfully',
+        });
+      }
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    req.end();
+  });
+};
+
+cron.schedule('*/10 * * * *', async () => {
+  try {
+    const response = await exports.handler();
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
 });
